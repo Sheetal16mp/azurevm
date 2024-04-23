7a855b7f589af449ae7ab6d47d65e87d8bc403e4
Sheetal16mp <159913254+Sheetal16mp@users.noreply.github.com>
2024-04-23 15:48:30 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 # We strongly recommend using the required_providers block to set the  
 # Azure Provider source and version being used
 terraform {
-  required_providers {
+ required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.100.0"
+        source = "hashicorp/azurerm"
+        version = "=2.75.0"
     }
   }
 }
